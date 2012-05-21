4aebc8c0367fd1ad82603e0200e8bb591b2b0ce9
Tim McIver <tim@timmciver.com>
2012-05-21 13:26:10 -0400

    Added function 'core/run-processors' which loops over all processors to produce a Ring response. Seems to work for a simple :get request, but not otherwise tested.

--- src/hottop/core.clj
+++ src/hottop/core.clj
@@ -3,4 +3,22 @@
 
 (def ^:private processors [process-options
                            validate-method
-                           validate-authorization])
+                           validate-authorization
+                           process-acceptable-media-types
+                           process-request])
+
+(defn run-processors
+  "Runs the processors on the given resource and request to produce a response."
+  [resource request]
+  (loop [processors processors
+         resource resource
+         request request
+         response {}
+         handlers {}]
+    (if-let [processor (first processors)]
+      (let [[resource request response handlers] (processor resource request response handlers)]
+        (if (nil? (:status response))
+          (recur (rest processors) resource request response handlers)
+          response))
+      {:status 500
+       :body "Internal Server Error"})))
