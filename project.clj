9ae451ee50572a28dc614e09bb50c45e7b244b9b
Tim McIver <tim@timmciver.com>
2013-01-01 17:11:17 -0500

    Updated project.clj to be lein2 compatible. Added target directory to list of patterns to be ignored in .gitignore.

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
-(defproject hottop "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject hottop/hottop "1.0.0-SNAPSHOT" 
+  :min-lein-version "2.0.0"
+  :profiles {:dev
+             {:dependencies
+              [[ring-mock "0.1.2"]
+               [ring/ring-jetty-adapter "1.0.0-RC1"]
+               [hiccup "1.0.0"]]}}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [ring/ring-core "1.0.0-RC1"]
                  [clout "1.0.1"]]
-  :dev-dependencies [[ring-mock "0.1.2"]
-                     [ring/ring-jetty-adapter "1.0.0-RC1"]
-                     [hiccup "1.0.0"]])
+  :description "HoTToP is a resource-oriented web application framework.")
