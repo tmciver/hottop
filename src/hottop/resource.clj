ab43f42d18992df2fcce09cb34b2efaffb7f68f2
Tim McIver <tim@timmciver.com>
2013-01-06 10:32:13 -0500

    Fixed resource doc comments.

--- src/hottop/resource.clj
+++ src/hottop/resource.clj
@@ -3,17 +3,11 @@
 ;; A resource is a regular Clojure map and can contain the following keys and
 ;; values:
 ;;
-;; :uri
-;;   A clout vector representing the URI of the resource.
-;; :auth
-;;   A function whose only argument is the Ring request and which should
-;;   return true is the user is authorized to access this resource, false
-;;   otherwise.
 ;; :content-types-provided
 ;;   A map whose keys are strings of internet media types (see
 ;;   http://en.wikipedia.org/wiki/Internet_media_type for valid media types) to
 ;;   functions of a single argument. These functions are intended to convert the
-;;   data returned by the method functions (see below) to the format given by
+;;   data returned by the :method functions (see below) to the format given by
 ;;   the media type.
 ;; :content-types-accepted
 ;;   A map whose keys are strings of internet media types (see
@@ -22,18 +16,21 @@
 ;;   data from the client into a more usable format (This needs to be defined
 ;;   better).
 ;; :languages-provided
-;;   A vector of strings representing language codes. Defaults to ["en"
-;;   "en-us"].
+;;   A set of strings representing language codes. Defaults to #{"en" "en-us"}.
 ;; :methods
 ;;   A map of HTTP method keywords to functions. The function associated with a
 ;;   method should take one argument which will be the Ring request. The return
 ;;   value can be any application-specific data structure. The returned data
 ;;   structure will be passed to the function associated with the optimal
 ;;   internet media type found in the :content-types-provided map.
+;; :auth
+;;   A function whose only argument is the Ring request and which should
+;;   return true if the user is authorized to access this resource, false
+;;   otherwise.
 (def base-resource {:content-types-provided {}
                     :content-types-accepted {"application/x-www-form-urlencoded" 'some-fn-to-parse-like-from-Ring}
                     :auth (constantly true)
-                    :languages-provided ["en" "en-us"]
+                    :languages-provided #{"en" "en-us"}
                     :methods {}})
 
 (defn create-readonly-html-resource
