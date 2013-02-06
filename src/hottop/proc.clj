40dcbe218426dd8a777cc2d583e19e977b72c0a5
Tim McIver <tim@timmciver.com>
2013-02-05 23:02:17 -0500

    Fixed github issue #5: add Content-Type header to response to GET request.

--- src/hottop/proc.clj
+++ src/hottop/proc.clj
@@ -108,7 +108,8 @@ been used."
     (if-let [ct-fn (get-in resource [:content-types-provided ct-desired])]
       (let [get-fn (get-in resource [:methods :get])
             result (ct-fn (get-fn request))]
-        (ring/response result))
+        (-> (ring/response result)
+            (ring/header "content-type" ct-desired)))
       (response/code 500))
     (response/code 406)))
 
