69b75f56aea8c6e28c6ec9247e420198dab13e6a
Tim McIver <tim@timmciver.com>
2013-02-07 13:29:44 -0500

    Removed files and directories under and including src/hottop/samples. Removed unnecessary dependencies from project.clj.

--- project.clj
+++ project.clj
@@ -2,9 +2,7 @@
   :min-lein-version "2.0.0"
   :profiles {:dev
              {:dependencies
-              [[ring-mock "0.1.2"]
-               [ring/ring-jetty-adapter "1.0.0-RC1"]
-               [hiccup "1.0.0"]]}}
+              [[ring-mock "0.1.2"]]}}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [ring/ring-core "1.0.0-RC1"]
                  [clout "1.0.1"]]
