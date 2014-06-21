ef606f796ce313a94f00713eba8ac0c8cd0ea0b1
Tim McIver <tim@timmciver.com>
2014-06-20 22:58:57 -0400

    Update Ring dep from 1.0.0-RC1 to 1.3.0.

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :min-lein-version "2.0.0"
   :profiles {:dev {:dependencies [[ring-mock "0.1.2"]]}}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [ring/ring-core "1.0.0-RC1"]
+                 [ring/ring-core "1.3.0"]
                  [clout "1.0.1"]]
   :description "HoTToP is a resource-oriented web application framework.")
