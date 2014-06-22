5f5712271ad478276753070f9d73a490afcbef11
Tim McIver <tim@timmciver.com>
2014-06-21 22:24:16 -0400

    Refactored hottop.middleware to use facilities in ring.util.mime-type. Resolves #6.

--- test/hottop/test/middleware.clj
+++ test/hottop/test/middleware.clj
@@ -4,26 +4,15 @@
         hottop.middleware))
 
 (deftest test-wrap-accept-from-extension
-  (let [handler (-> (fn [req]
-                      ;; just return the request as response
-                      req)
-                    wrap-accept-from-extension)
+  (let [handler (wrap-accept-from-extension (fn [req] req))
         req1 (request :get "/test")
         req2 (request :get "/test.zip")
         req3 (request :get "/test.html")
         req4 (request :get "/test.csv")
         req5 (request :get "/test.abc")
         get-accept (fn [req] (get-in req [:headers "accept"]))]
-    (is (let [resp (handler req1)]
-          (and (= resp req1)
-               (= (:ext resp) nil))))
-    (is (let [resp (handler req2)]
-          (and (= (get-accept resp) "application/zip")
-               (= (:ext resp) "zip"))))
-    (is (let [resp (handler req3)]
-          (and (= (get-accept resp) "text/html")
-               (= (:ext resp) "html"))))
-    (is (let [resp (handler req4)]
-          (and (= (get-accept resp) "text/csv")
-               (= (:ext resp) "csv"))))
-    (is (= (handler req5) {:status 406 :body "Unsupported file extension"}))))
+    (is (= (handler req1) req1))
+    (is (= (get-accept (handler req2)) "application/zip"))
+    (is (= (get-accept (handler req3)) "text/html"))
+    (is (= (get-accept (handler req4)) "text/csv"))
+    (is (= (:status (handler req5)) 406))))
